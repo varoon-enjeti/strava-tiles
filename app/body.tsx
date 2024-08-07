8f0323267975f58599a00eed784e98e108c5c864
varoon-enjeti <varoon.enjeti@gmail.com>
2024-08-07 01:08:15 -0700

    date grid working lfg

--- app/body.tsx
+++ app/body.tsx
@@ -4,10 +4,21 @@ import Tile from "./tile";
 export default function Body() {
 	const raw = [];
 
+	const today = new Date();
+	const reference = new Date();
+	reference.setDate(today.getDate() - today.getDay() - 364);
+
 	for (let i = 0; i < 7; i++) {
 		const row = [];
+		// const curr = new Date();
+		// curr.setDate(reference.getDate() + i);
 		for (let j = 0; j < 52; j++) {
-			row.push(<Tile />);
+			const inst = new Date();
+			inst.setDate(reference.getDate() + j * 7 + i);
+			inst.setFullYear(inst.getFullYear() - 1);
+			row.push(<Tile date={inst} />);
+			// curr.setDate(curr.getDate() + 7);
+			console.log(inst.toDateString() + "\n");
 		}
 		raw.push(row);
 	}
@@ -17,15 +28,15 @@ export default function Body() {
 		format.push(<div className="flex gap-1">{row}</div>);
 	}
 
-	const date = new Date();
-
-	const pastDate = new Date(date);
-	pastDate.setDate(date.getDate() - 365);
+	const test1 = new Date();
+	test1.setDate(reference.getDate() + 7);
+	const test2 = new Date();
+	test2.setDate(reference.getDate() + 7);
 
 	return (
 		<div className="w-full h-full flex flex-col gap-1 justify-center items-center">
 			{format}
-			<div className="">{pastDate.toUTCString()}</div>
+			<div className="">{test1.toDateString()}</div>
 		</div>
 	);
 }
--- app/tile.tsx
+++ app/tile.tsx
@@ -4,8 +4,11 @@ interface DayTile {
 
 export default function Tile({ date }: DayTile) {
 	return (
-		<div className="w-3 h-3 bg-gray-300 rounded">
-			<p className=""></p>
+		<div className="w-3 h-3 rounded group">
+			<div className="w-full h-full bg-gray-300 rounded"></div>
+			<span className="absolute scale-0 w-20 h-5 -mt-10 rounded bg-gray-300 border-[1px] border-gray-900 text-xs group-hover:scale-100 flex items-center justify-center">
+				{date.toLocaleDateString()}
+			</span>
 		</div>
 	);
 }
