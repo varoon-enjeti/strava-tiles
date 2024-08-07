a2b5cb7ea5176c55f66babf4b2ff03baad25f894
varoon-enjeti <varoon.enjeti@gmail.com>
2024-08-06 21:26:37 -0700

    basic year grid

--- app/body.tsx
+++ app/body.tsx
@@ -1,9 +1,25 @@
 import { GetServerSideProps } from "next";
+import Tile from "./tile";
 
 export default function Body() {
+	const raw = [];
+
+	for (let i = 0; i < 7; i++) {
+		const row = [];
+		for (let j = 0; j < 52; j++) {
+			row.push(<Tile />);
+		}
+		raw.push(row);
+	}
+
+	const format = [];
+	for (const row of raw) {
+		format.push(<div className="flex gap-1">{row}</div>);
+	}
+
 	return (
-		<div className="w-full h-full flex justify-center items-center">
-			Hello World
+		<div className="w-full h-full flex flex-col gap-1 justify-center items-center">
+			{format}
 		</div>
 	);
 }
---
+++ app/tile.tsx
@@ -0,0 +1,7 @@
+export default function Tile() {
+	return (
+		<div className="w-3 h-3 bg-gray-300 rounded">
+			<p className=""></p>
+		</div>
+	);
+}
