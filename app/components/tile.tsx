c529e1101131cf7a4a494590aaf8517b167193e4
varoon-enjeti <varoon.enjeti@gmail.com>
2025-04-14 17:08:27 -0500

    transition load in

--- app/components/tile.tsx
+++ app/components/tile.tsx
@@ -34,7 +34,7 @@ export default function Tile({ date, activities, isLoaded }: TileProps) {
 	return (
 		<div className="relative w-3 h-3 group">
 			<div
-				className={`w-full h-full ${color} transition-colors duration-700n rounded`}
+				className={`w-full h-full ${color} transition-colors duration-700 rounded`}
 			></div>
 
 			<div className="absolute left-0 bottom-0 translate-y-[0px] scale-0 group-hover:scale-100 transition-transform z-10 bg-gray-300 border border-gray-900 text-xs rounded p-1 flex flex-col items-start w-max max-w-[10rem] whitespace-nowrap shadow-lg">
